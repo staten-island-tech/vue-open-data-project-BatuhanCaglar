9a2b2efc114f7aa719bbb1f2005d001500a3f67c
BatuhanCaglar <fbmanbc@gmail.com>
2025-03-05 14:01:10 -0500

    brih

--- app/src/components/EvictData.vue
+++
@@ -1,5 +0,0 @@
-<script setup></script>
-
-<style scoped></style>
-
-<template></template>
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,42 +1,10 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
 <template>
-  <div class="data-container">
-    <h1 class="web-title">Evictions Extravaganza</h1>
-    <button class="start-button">Analyze</button>
-  </div>
+  <h1>Evictions Extravaganza</h1>
+  <main>
+    <TheWelcome />
+  </main>
 </template>
-
-<script setup></script>
-
-<style>
-.game-container {
-  background: rgba(75, 86, 144, 0.7);
-  padding: 40px;
-  border-radius: 15px;
-  text-align: center;
-  width: 60%;
-  margin: auto;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
-
-.game-title {
-  font-family: 'Handjet', sans-serif;
-  font-optical-sizing: auto;
-  font-size: 3rem;
-  margin-bottom: 30px;
-}
-.start-button {
-  background-color: orange;
-  padding: 20px 40px;
-  border: none;
-  border-radius: 15px;
-  cursor: pointer;
-  font-size: 2rem;
-  display: block;
-  margin: 0 auto;
-  font-family: 'Handjet', sans-serif;
-  font-optical-sizing: auto;
-}
-</style>
