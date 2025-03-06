d154ba2f55a662778998c4aafee728d4ee3b51d6
BatuhanCaglar <fbmanbc@gmail.com>
2025-03-06 13:04:48 -0500

    bruh

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,33 +1,29 @@
 <template>
-  <h1>Evictions Extravaganza </h1>
   <div class="container">
-    <h1>Eviction Records</h1>
+    <h1>Evictions Extravaganza</h1>
     <div class="card-container">
       <EvictionCard v-for="(eviction, index) in evictions" :key="index" :eviction="eviction" />
     </div>
   </div>
 </template>
 
-
 <script setup>
-import { ref, onMounted } from 'vue';
-import EvictionCard from '../components/EvictionCard.vue';
+import { ref, onMounted } from 'vue'
+import EvictionCard from '../components/EvictionCard.vue'
 
-const evictions = ref([]);
+const evictions = ref([])
 
-const fetchEvictions = async () => {
+async function fetchEvictions() {
   try {
-    const response = await fetch('https://data.cityofnewyork.us/resource/6z8x-wfk4.json');
-    evictions.value = await response.json();
+    const response = await fetch('https://data.cityofnewyork.us/resource/6z8x-wfk4.json')
+    evictions.value = await response.json()
   } catch (error) {
-    console.error('Error fetching eviction data:', error);
+    console.error('Error fetching eviction data:', error)
   }
-};
+}
 
-onMounted(fetchEvictions);
+onMounted(fetchEvictions)
 </script>
-
-
 
 <style scoped>
 .container {
